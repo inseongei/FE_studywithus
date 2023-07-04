64ff50f2f8d7a9adf715573d4fb94e42a71daaec
inseongei <twin2688@naver.com>
2023-07-05 00:43:53 +0900

    팀원 리스트 수정

--- src/compontents/Team.js
+++ src/compontents/Team.js
@@ -1,43 +1,17 @@
 import React from 'react'
 import styled from "styled-components"
-import { MdDateRange } from "react-icons/md";
 
 const Team = () => {
   return (
     <Container>
         <div className='Cardbox'>
             <div className="imgbox">사진</div>
-
             <div className='subbox'>
-
-
-
-
-
-
-            <div className='titlebox'> 스프링 기반 프로젝트 참여하고 싶습니다</div>
-
-
+            <div className='titlebox'> 홍길동</div>
             <div className='datebox'>
-            <div className='dateinfo'>
-              <div className='iconbox'><MdDateRange></MdDateRange></div>
-              <div>2023-07-04</div>
-            </div>
             <div className='who'>👨‍🔧개발자</div>
-
-
             </div>
-
-
-            <div className='contentbox'>나는 내용이고 너도 내용이고 우리도 내용이고 언제나 내용이고 이거는 내용이고 저거도 내용이고나는 내용이고 너도 내용이고 우리도 내용이고 언제나는 내용이고 너도 내용이고 우리도 내용이고 언제나 내용이고 이거는 내용이고 저거도 내용이고나는 내용이고 너도 내용이고 우리도 내용이고 언제나 내용이고 이거는 내용이고 저거도 내용이고나는 내용이고 너도 내용이고 우리도 내용이고 언제나 내용이고 이거는 내용이고 저거도 내용이고나는 내용이고 너도 내용이고 우리도 내용이고 언제나 내용이고 이거는 내용이고 저거도 내용이고나는 내용이고 너도 내용이고 우리도 내용이고 언제나 내용이고 이거는 내용이고 저거도 내용이고나는 내용이고 너도 내용이고 우리도 내용이고 언제나 내용이고 이거는 내용이고 저거도 내용이고나는 내용이고 너도 내용이고 우리도 내용이고 언제나 내용이고 이거는 내용이고 저거도 내용이고나 나는 내용이고 너도 내용이고 우리도 내용이고 언제나 내용이고 이거는 내용이고 저거도 내용이고내용이고 이거는 내용이고나는 내용이고 너도 내용이고 우리도 내용이고 언제나 내용이고 이거는 내용이고 저거도 내용이고 저거도 내용이고</div>
-
-
-
-
-
-
-
-
+            <div className='contentbox'>사용 스택</div>
             </div>
         </div>
     </Container>
@@ -48,14 +22,14 @@ const Container = styled.div`
 width:70%;
 margin: auto;
 height: 200px;
-margin-bottom: 20px;
-
+margin-bottom: 30px;
 
 .Cardbox{
 height: 200px;
 display: flex;
 background-color: #CDCFEA;
 border-radius: 8px;
+
 }
 
 .imgbox{
@@ -88,11 +62,6 @@ border-radius: 8px;
   margin-right: 15px;
 }
 
-.iconbox{
-  display: flex;
-  align-items: center;
-}
-
 .who{
   font-weight: 700;
 }
@@ -102,6 +71,8 @@ border-radius: 8px;
   padding: 7px;
   height: 80px;
   overflow: hidden;
+  border-radius: 8px;
+  background-color: #EBEEFF;
 }
 
 
