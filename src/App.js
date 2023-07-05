3a6a511ef602f9bb495e720e66d560eb58514905
inseongei <twin2688@naver.com>
2023-07-05 11:22:11 +0900

    스케줄 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ProjectDetail from './pages/ProjectDetail';
 import ProjectWrite from './pages/ProjectWrite';
 import { Routes, Route } from 'react-router-dom'
 import ProjectMain from './pages/ProjectMain';
+import Schedule from './pages/Schedule';
 
 
 function App() {
