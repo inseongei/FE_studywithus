7f459d14e900a36dfce17f5ee35df1fd41c0d8e3
inseongei <twin2688@naver.com>
2023-07-06 00:56:26 +0900

    라우팅

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import { Routes, Route } from 'react-router-dom'
 import ProjectMain from './pages/ProjectMain';
 import Schedule from './pages/Schedule';
 import Portfolio from './pages/Portfolio'
+import PortfolioDetail from './pages/PortfolioDetail';
+import PortfolioWrite from './pages/PortfolioWrite'
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path="/" element={<Main/>} />
+      <Route path="/" element={<PortfolioWrite/>} />
       <Route path="/Login" element={<Login/>} />
       <Route path="/ProjectMain" element={<ProjectMain/>} />
       <Route path="/ProjectWrite" element={<ProjectWrite/>} />
