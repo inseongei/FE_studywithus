913ec4ae0c31cf7c717ea74ce1e26ca1efe6b1e8
inseongei <twin2688@naver.com>
2023-07-07 21:57:44 +0900

    팀 카드 수정

--- src/compontents/Team.js
+++ src/compontents/Team.js
@@ -5,7 +5,9 @@ const Team = () => {
   return (
     <Container>
         <div className='Cardbox'>
-            <div className="imgbox">사진</div>
+            <div className="imgbox">
+              <img src="https://upload.wikimedia.org/wikipedia/ko/4/4a/%EC%8B%A0%EC%A7%B1%EA%B5%AC.png" alt="사진"/>
+            </div>
             <div className='subbox'>
             <div className='titlebox'> 홍길동</div>
             <div className='datebox'>
@@ -27,15 +29,23 @@ margin-bottom: 30px;
 .Cardbox{
 height: 200px;
 display: flex;
-background-color: #CDCFEA;
+background-color: #d9f0e6;
 border-radius: 8px;
+cursor: pointer;
+}
 
+.Cardbox:hover{
+  transform: scale(1.01);
 }
 
 .imgbox{
-  border: 1px solid black;
   border-radius: 8px;
   width:15%;
+  height: 200px;
+}
+
+.imgbox img {
+  width: 100%;
   height: 200px;
 }
 
@@ -72,7 +82,7 @@ border-radius: 8px;
   height: 80px;
   overflow: hidden;
   border-radius: 8px;
-  background-color: #EBEEFF;
+  background-color: #fff;
 }
 
 
