93aa684cf9f72c03d0eb95e75a7a7c21d705afba
inseongei <twin2688@naver.com>
2023-07-19 00:04:51 +0900

    리코일 설정 삭제

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <RecoilRoot>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-    </RecoilRoot>
 );
