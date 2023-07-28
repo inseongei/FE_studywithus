e4cb4c3e00f62aae50326fb68c41ff2b30a4ac3a
inseongei <twin2688@naver.com>
2023-07-28 19:24:11 +0900

    리덕스 세팅

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import {Provider} from "react-redux"
+import {createStore} from "redux"
+import {reducer} from './store/reducer'
 
+const store = createStore(reducer)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Provider store={store}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </Provider>
 );
