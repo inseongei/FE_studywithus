b36d9903480b17e75ea9c019eb7a6f71cc9ea195
inseongei <twin2688@naver.com>
2023-07-29 14:31:24 +0900

    파이어베이스 초기 설정

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -1,16 +1,15 @@
-import firebase from 'firebase/app'
-import database from 'firebase/database'
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
 
-// firebase 연결 구성 함수 (initializeApp)
 const firebaseConfig = {
-    apiKey : 'AIzaSyD_BsDXdRPTpPKxvsFEWX7rfWJ4bLcoG-w',
-    databaseURL : 'https://meet-aa3bd-default-rtdb.asia-southeast1.firebasedatabase.app/' 
-}
-firebase.initializeApp(firebaseConfig)
+  apiKey: "AIzaSyCSTI45lwXoITi8m6eU3kP85EyNEjEbb4A",
+  authDomain: "studywithus-d198c.firebaseapp.com",
+  projectId: "studywithus-d198c",
+  storageBucket: "studywithus-d198c.appspot.com",
+  messagingSenderId: "971102011034",
+  appId: "1:971102011034:web:a7974d7ea262412e75e923"
+};
 
-// firebase 실시간 데이터베이스에 참조하기 위해 사용되는 메서드
-export let dbRef = firebase.database().ref(); 
-// firebase 실시간 데이터베이스에 연결 상태를 감지한다. (info/connected) 경로는 연결 상태를 나타내는 특별한 경로
-export let connectRef = firebase.database().ref(".info/connected");
-
-export const userName = localStorage.getItem('userName')
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app) 
